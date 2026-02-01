a5432635e12f8bcbc1a1e60054d038cd7be877ee
devansh05 <devansh2393@gmail.com>
2026-02-01 23:18:43 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const PORT = 3000;
+const app = express();
+
+app.get("/", (req, res, next) => {
+  res.send("INITIATED AND RUNNING.");
+});
+
+app.listen(PORT, () => console.log(`🟡 LOG - LISTENING ON PORT : `, PORT));
